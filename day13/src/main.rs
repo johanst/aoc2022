6710fb9f624533370e4e0efa3dd39d782e31cb13
johanst <johan.sternerup@gmail.com>
2022-12-13 09:07:18 +0100

    day13 data collected

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -8,6 +8,8 @@ use std::collections::HashMap;
 use std::cmp;
 use std::fmt;
 use std::io::stdin;
+use serde::{Deserialize, Serialize};
+use serde_json::{Result, Value};
 
 enum NodeType {
     Node(usize),
@@ -17,11 +19,11 @@ enum NodeType {
 struct Node {
     left : NodeType,
     right : NodeType
-};
+}
 
 impl Node {
     fn to_string(&self, ns : &Vec<Node>) -> String {
-        "".to_string();
+        "".to_string()
     }
 }
 
@@ -40,7 +42,15 @@ fn main() {
     }
     let v = v;
 
-    dbg!(v);
+    let mut data: Vec::<(Value, Value)> = Vec::new();
+    for d in v.chunks_exact(3) {
+        let mut itr = d.iter();
+        let a = serde_json::from_str(itr.next().unwrap()).unwrap();
+        let b = serde_json::from_str(itr.next().unwrap()).unwrap();
+        data.push((a, b));
+    }
+
+    dbg!(data);
 
     part1();
     part2();
@@ -51,7 +61,11 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_lit_plus_lit() {
+    fn test_from_string() {
+        let data = "[[1],[2,3,4]]";
+        let v: Value = serde_json::from_str(data).unwrap();
+        let s: String = serde_json::to_string(&v).unwrap();
+        assert_eq!(data, s);
     }
 }
 
