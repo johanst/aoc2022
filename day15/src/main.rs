074b7d74321050d46edbd342882ecb94e7654866
johanst <johan.sternerup@gmail.com>
2022-12-15 07:22:14 +0100

    day15 parsed it

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -9,14 +9,29 @@ use std::cmp;
 use std::fmt;
 use std::io::stdin;
 
+#[derive(Debug, Clone, Copy)]
+struct Sensor {
+    sx : i32,
+    sy : i32,
+    by : i32,
+    bx : i32,
+}
+
+fn to_i32(s : &str) -> i32 {
+    let sn = s.chars()
+        .filter(|c| *c == '-' || c.is_numeric())
+        .collect::<String>();
+    sn.parse::<i32>().unwrap()
+}
+
 fn part1() {
 }
 
 fn part2() {
 }
 
-fn main() {
-    let lines = std::fs::read_to_string("ex.txt").unwrap();
+fn read_input(f : &str) -> Vec<Sensor> {
+    let lines = std::fs::read_to_string(f).unwrap();
     let mut v = lines.split("\n").collect::<Vec<&str>>();
     assert!(!v.is_empty());
     if v[v.len() - 1] == "" {
@@ -24,7 +39,23 @@ fn main() {
     }
     let v = v;
 
-    dbg!(v);
+    let mut m : Vec<Sensor> = Vec::new();
+    for row in v.iter() {
+        let w = row.split_whitespace().collect::<Vec<&str>>();
+        m.push(Sensor {
+            sx : to_i32(w[2]),
+            sy : to_i32(w[3]),
+            bx : to_i32(w[8]),
+            by : to_i32(w[9]),
+        });
+    }
+
+    m
+}
+
+fn main() {
+    let m : Vec<Sensor> = read_input("ex.txt");
+    //dbg!(m);
 
     part1();
     part2();
