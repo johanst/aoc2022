f86cbd8987e44137053f0670054d594bbeba00ef
johanst <johan.sternerup@gmail.com>
2022-12-16 10:58:57 +0100

    day16 read idx

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -16,7 +16,7 @@ fn part2() {
 }
 
 fn main() {
-    let lines = std::fs::read_to_string("input.txt").unwrap();
+    let lines = std::fs::read_to_string("ex.txt").unwrap();
     let mut v = lines.split("\n").collect::<Vec<&str>>();
     assert!(!v.is_empty());
     if v[v.len() - 1] == "" {
@@ -24,13 +24,16 @@ fn main() {
     }
     let v = v;
 
-    let mut vid2idx : HashMap(<&str, usize>) = HashMap::new();
-    v.iter().enumerate()
-        .for_each(|n vid| {
-            vi2idx.insert()
-                vid.split_whitespace().skip(1).take(1)
+    let mut vid2idx : HashMap<&str, usize> = HashMap::new();
+    let mut idx2vid : Vec<&str> = Vec::new();
+    for (n, row) in v.iter().enumerate() {
+        let vid = row.split_whitespace().skip(1).next().unwrap();
+        vid2idx.insert(vid, n);
+        idx2vid.push(vid);
+    }
 
-    dbg!(&v, v.len());
+    dbg!(vid2idx);
+    dbg!(idx2vid);
 
     part1();
     part2();
