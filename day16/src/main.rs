966b09abdfb8a866b015aba5c2e005984d01db71
johanst <johan.sternerup@gmail.com>
2022-12-16 16:40:52 +0100

    data read

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -9,6 +9,60 @@ use std::cmp;
 use std::fmt;
 use std::io::stdin;
 
+#[derive(Debug, Default, Clone)]
+struct Node {
+    rate : u32,
+    paths : Vec<usize>
+}
+
+#[derive(Debug)]
+struct State {
+    vid2idx : HashMap<String, usize>,
+    idx2vid : Vec<String>,
+    map : Vec<Node>,
+}
+
+impl State {
+    fn new(v : &Vec<&str>) -> Self {
+        let mut st = State {
+            vid2idx : HashMap::new(),
+            idx2vid : Vec::new(),
+            map : Vec::new(),
+        };
+        for (n, row) in v.iter().enumerate() {
+            let vid = row.split_whitespace().skip(1).next().unwrap();
+            st.vid2idx.insert(vid.to_string(), n);
+            st.idx2vid.push(vid.to_string());
+        }
+
+        st.map = vec![Node::default(); st.idx2vid.len()];
+
+        for (n, row) in v.iter().enumerate() {
+            let paths = row.split_whitespace()
+                .skip(9)
+                .map(|s| st.vid2idx.get(&s[0..2]).unwrap()).cloned()
+                .collect::<Vec<usize>>();
+            let rate = row.split_whitespace()
+                .skip(4).next().unwrap()
+                .split("=")
+                .skip(1).next().unwrap()
+                .split(";").take(1).next().unwrap()
+                .parse::<u32>().unwrap();
+            st.map[n] = Node { rate, paths };
+        }
+
+        st
+    }
+
+    fn vid2idx(&self, s : &str) -> usize {
+        self.vid2idx.get(s).unwrap().clone()
+    }
+
+    fn idx2vid(&self, idx : usize) -> String {
+        self.idx2vid[idx].clone()
+    }
+}
+
 fn part1() {
 }
 
@@ -24,16 +78,9 @@ fn main() {
     }
     let v = v;
 
-    let mut vid2idx : HashMap<&str, usize> = HashMap::new();
-    let mut idx2vid : Vec<&str> = Vec::new();
-    for (n, row) in v.iter().enumerate() {
-        let vid = row.split_whitespace().skip(1).next().unwrap();
-        vid2idx.insert(vid, n);
-        idx2vid.push(vid);
-    }
+    let st = State::new(&v);
 
-    dbg!(vid2idx);
-    dbg!(idx2vid);
+    dbg!(st);
 
     part1();
     part2();
