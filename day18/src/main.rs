843512ed8db73189ff86e29c84a9f627a45ea180
johanst <johan.sternerup@gmail.com>
2022-12-18 21:55:46 +0100

    close to solving it

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -22,7 +22,7 @@ fn part2() {
 }
 
 fn main() {
-    let lines = std::fs::read_to_string("input.txt").unwrap();
+    let lines = std::fs::read_to_string("ex.txt").unwrap();
     let mut v = lines.split("\n").collect::<Vec<&str>>();
     assert!(!v.is_empty());
     if v[v.len() - 1] == "" {
@@ -32,6 +32,7 @@ fn main() {
 
     let mut mcubes : HashMap<(i32,i32,i32), usize> = HashMap::new();
     let mut cubes : Vec<Cube> = Vec::new();
+    let mut adj_cubes : HashSet<(i32,i32,i32)> = HashSet::new();
     for sc in v.iter() {
         let coords = sc.split(",")
             .map(|w| w.parse::<i32>().unwrap())
@@ -47,6 +48,8 @@ fn main() {
             let adj_pos = (from_pos.0 + dx, from_pos.1 + dy, from_pos.2 + dz);
             if let Some(idx_to) = mcubes.get(&adj_pos) {
                 cubes[idx_from].adj.push(*idx_to);
+            } else {
+                adj_cubes.insert(adj_pos);
             }
         }
     }
@@ -56,8 +59,51 @@ fn main() {
 
     dbg!(num_free);
 
-    part1();
-    part2();
+    // part 2
+
+    //dbg!(adj_cubes);
+    let (mut xmax, mut ymax, mut zmax) = (0i32, 0i32, 0i32);
+    for cube in cubes.iter() {
+        xmax = cmp::max(xmax, cube.pos.0);
+        ymax = cmp::max(ymax, cube.pos.1);
+        zmax = cmp::max(zmax, cube.pos.2);
+    }
+    //dbg!(xmax, ymax, zmax);
+
+    // known => true =
+    let mut contained : HashSet<(i32,i32,i32)> = HashSet::new();
+    let mut open_space : HashSet<(i32,i32,i32)> = HashSet::new();
+    for (xstart, ystart, zstart) in adj_cubes {
+        let mut visited : HashSet<(i32,i32,i32)> = HashSet::new();
+        let mut inspect_cubes : VecDeque<(i32,i32,i32)> = VecDeque::new();
+        inspect_cubes.push_back((xstart, ystart, zstart));
+        // until we know the state of our
+        let mut is_open_space = false;
+        while !inspect_cubes.is_empty() {
+            let (x, y, z) = inspect_cubes.pop_front().unwrap();
+            visited.insert((x, y, z));
+            if x == 0 || x >= xmax || y == 0 || y >= ymax || z == 0 || z >= zmax {
+                is_open_space = true;
+                continue;
+            }
+            let offset = [(-1, 0, 0), (1, 0, 0), (0, -1, 0), (0, 1, 0), (0, 0, -1), (0, 0, 1)];
+            for (dx, dy, dz) in offset {
+                if !(visited.contains(&(x + dx, y + dy, z + dz)) ||
+                    mcubes.contains_key(&(x + dx, y + dy, z + dz))) {
+                        inspect_cubes.push_back((x + dx, y + dy, z + dz));
+                    }
+            }
+        }
+
+        if is_open_space {
+            open_space.extend(visited);
+        } else {
+            contained.extend(visited);
+        }
+    }
+
+    dbg!(contained);
+    dbg!(open_space.len());
 }
 
 #[cfg(test)]
