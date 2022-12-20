2e0d8cebfba8c7d2a4c7ea5e82771b41dee99d27
johanst <johan.sternerup@gmail.com>
2022-12-20 22:53:44 +0100

    day19 sed next

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -9,6 +9,25 @@ use std::cmp;
 use std::fmt;
 use std::io::stdin;
 
+#[derive(Debug, Default)]
+struct Config {
+    ore_rc_ore : u32,
+    clay_rc_ore : u32,
+    obs_rc_ore : u32,
+    obs_rc_clay : u32,
+    geo_rc_ore : u32,
+    geo_rc_obs : u32,
+}
+
+fn get_numeric(s: &str, idx : usize) -> u32 {
+    let mut w = s.split_whitespace();
+    if idx == 0 {
+        return w.next().unwrap().parse::<u32>().unwrap();
+    } else {
+        return w.skip(idx).next().unwrap().parse::<u32>().unwrap();
+    }
+}
+
 fn part1() {
 }
 
@@ -17,13 +36,26 @@ fn part2() {
 
 fn main() {
     let lines = std::fs::read_to_string("ex.txt").unwrap();
-    let mut v = lines.split("\n").collect::<Vec<&str>>();
+    let v = lines.split("\n").collect::<Vec<&str>>();
     assert!(!v.is_empty());
-    if v[v.len() - 1] == "" {
-        v.pop();
-    }
-    let v = v;
+    let mut cfgs : Vec<Config> = Vec::new();
+    for w in v.chunks(6) {
+        let mut witr = w.iter();
+        witr.next();
+        let mut cfg = Config::default();
+        cfg.ore_rc_ore = get_numeric(witr.next().unwrap(), 4);
+        cfg.clay_rc_ore = get_numeric(witr.next().unwrap(), 4);
+        let obsrob = witr.next().unwrap();
+        cfg.obs_rc_ore = get_numeric(obsrob, 4);
+        cfg.obs_rc_clay = get_numeric(obsrob, 7);
+        let georob = witr.next().unwrap();
+        cfg.geo_rc_ore = get_numeric(georob, 4);
+        cfg.geo_rc_obs = get_numeric(georob, 7);
 
+        cfgs.push(cfg);
+    }
+
+    dbg!(cfgs);
     dbg!(v);
 
     part1();
