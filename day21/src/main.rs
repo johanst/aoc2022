faf6e3cf7f07a8db6d0b7cc1c813e1dd1c641dcf
johanst <johan.sternerup@gmail.com>
2022-12-21 16:20:26 +0100

    day21 working

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -9,7 +9,7 @@ use std::cmp;
 use std::fmt;
 use std::io::stdin;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Operation {
     Add(usize, usize),
     Sub(usize, usize),
@@ -18,16 +18,69 @@ enum Operation {
     Lit(i64),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct OpWithValue {
+    op : Operation,
+    val : Option<i64>,
+    tainted : bool,
+}
+
+struct OpExt {
     op : Operation,
     val : Option<i64>,
 }
 
-fn part1() {
+fn part1(mnam2id : &HashMap<String, usize>, ops : &mut Vec<OpWithValue>) {
+    let root_idx = *mnam2id.get("root").unwrap();
+    let result = calc(root_idx, ops);
+    println!("Monkey result: {result}");
 }
 
-fn part2() {
+fn calc_with_humn(idx : usize, humn_idx : usize, humn_val : i64, ops : &Vec<OpWithValue>) -> i64 {
+    let mut ops = ops.clone();
+    ops[humn_idx].val = Some(humn_val);
+    calc(idx, &mut ops)
+ }
+
+fn part2(mnam2id : &HashMap<String, usize>, mut ops : Vec<OpWithValue>) {
+    let root_idx = *mnam2id.get("root").unwrap();
+    let (lhs_idx, rhs_idx) = match ops[root_idx].op {
+        Operation::Add(l, r) => (l, r),
+        _ => unreachable!()
+    };
+    let humn_idx = *mnam2id.get("humn").unwrap();
+
+    let mut humn_high = 4000000000000i64;
+    let mut humn_low = 3000000000000i64;
+    let rhs_val = calc_with_humn(rhs_idx, humn_idx, 0, &ops);
+    dbg!(rhs_val);
+    let mut lhs_val = 0;
+    while (lhs_val != rhs_val) {
+        let humn_middle = (humn_high + humn_low) / 2;
+        lhs_val = calc_with_humn(lhs_idx, humn_idx, humn_middle, &ops);
+        if lhs_val > rhs_val {
+            humn_low = humn_middle;
+        } else if lhs_val < rhs_val {
+            humn_high = humn_middle;
+        } else {
+            println!("Say: {humn_middle}");
+        }
+    }
+    dbg!(lhs_val);
+    dbg!(rhs_val);
+
+//    let humn_arr : [i64; 2] = [4000000000000, 3000000000000];
+//    for humn_val in humn_arr {
+//        let mut ops_l = ops.clone();
+//        let mut ops_r = ops.clone();
+//        ops_l[humn_idx].val = Some(humn_val);
+//        ops_r[humn_idx].val = Some(humn_val);
+//        let result_l = calc(lhs_idx, &mut ops_l);
+//        let result_r = calc(rhs_idx, &mut ops_r);
+//        println!("Humn = {humn_val} =>");
+//        println!("   Left: {result_l}");
+//        println!("  Right: {result_r}");
+//    }
 }
 
 fn calc(idx : usize, ops : &mut Vec<OpWithValue>) -> i64 {
@@ -56,7 +109,7 @@ fn calc(idx : usize, ops : &mut Vec<OpWithValue>) -> i64 {
 }
 
 fn main() {
-    let lines = std::fs::read_to_string("ex.txt").unwrap();
+    let lines = std::fs::read_to_string("input.txt").unwrap();
     let mut v = lines.split("\n").collect::<Vec<&str>>();
     assert!(!v.is_empty());
     if v[v.len() - 1] == "" {
@@ -77,7 +130,7 @@ fn main() {
         let w = r.split_whitespace().collect::<Vec<&str>>();
         if w.len() < 4 {
             let val = w[1].parse::<i64>().unwrap();
-            ops.push(OpWithValue {op: Operation::Lit(val), val: Some(val)});
+            ops.push(OpWithValue {op: Operation::Lit(val), val: Some(val), tainted: false});
         } else {
             let (aidx, bidx) = (*mnam2id.get(w[1]).unwrap(), *mnam2id.get(w[3]).unwrap());
             let op = match w[2] {
@@ -87,7 +140,7 @@ fn main() {
                 "/" => Operation::Div(aidx, bidx),
                 _ => unreachable!(),
             };
-            ops.push(OpWithValue {op, val: None});
+            ops.push(OpWithValue {op, val: None, tainted: false});
         }
     }
 
@@ -96,12 +149,9 @@ fn main() {
     //dbg!(&ops);
     //dbg!(v);
 
-    let root_idx = *mnam2id.get("root").unwrap();
-    let result = calc(root_idx, &mut ops);
-    println!("Monkey result: {result}");
-
-    part1();
-    part2();
+    let ops2 = ops.clone();
+    //part1(&mnam2id, &mut ops);
+    part2(&mnam2id, ops2);
 }
 
 #[cfg(test)]
