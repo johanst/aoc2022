59c10dd1c5198097ef3c7457ee213289ba011b42
johanst <johan.sternerup@gmail.com>
2022-12-21 15:13:15 +0100

    day21 puzzle 1 done

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -9,16 +9,50 @@ use std::cmp;
 use std::fmt;
 use std::io::stdin;
 
-enum Operand<'a> {
-    Ref(<&'a str>)
+#[derive(Debug)]
+enum Operation {
+    Add(usize, usize),
+    Sub(usize, usize),
+    Mul(usize, usize),
+    Div(usize, usize),
+    Lit(i64),
+}
 
-struct Operation {
-    Add()
+#[derive(Debug)]
+struct OpWithValue {
+    op : Operation,
+    val : Option<i64>,
+}
 
 fn part1() {
 }
 
 fn part2() {
+}
+
+fn calc(idx : usize, ops : &mut Vec<OpWithValue>) -> i64 {
+    if let Some(val) = ops[idx].val {
+        return val;
+    } else {
+        let (a, b) = match ops[idx].op {
+            Operation::Add(a, b) => (a, b),
+            Operation::Sub(a, b) => (a, b),
+            Operation::Mul(a, b) => (a, b),
+            Operation::Div(a, b) => (a, b),
+            _ => unreachable!(),
+        };
+        let aval = calc(a, ops);
+        let bval = calc(b, ops);
+        let result = match ops[idx].op {
+            Operation::Add(a, b) => aval + bval,
+            Operation::Sub(a, b) => aval - bval,
+            Operation::Mul(a, b) => aval * bval,
+            Operation::Div(a, b) => aval / bval,
+            _ => unreachable!(),
+        };
+        ops[idx].val = Some(result);
+        result
+    }
 }
 
 fn main() {
@@ -30,7 +64,41 @@ fn main() {
     }
     let v = v;
 
-    dbg!(v);
+    let mut mnams : Vec::<String> = Vec::new();
+    let mut mnam2id : HashMap<String, usize> = HashMap::new();
+    for (idx, r) in v.iter().enumerate() {
+        let mnam = r.split(":").next().unwrap().to_string();
+        mnam2id.insert(mnam.clone(), mnams.len());
+        mnams.push(mnam);
+    }
+
+    let mut ops : Vec<OpWithValue> = Vec::new();
+    for r in v.iter() {
+        let w = r.split_whitespace().collect::<Vec<&str>>();
+        if w.len() < 4 {
+            let val = w[1].parse::<i64>().unwrap();
+            ops.push(OpWithValue {op: Operation::Lit(val), val: Some(val)});
+        } else {
+            let (aidx, bidx) = (*mnam2id.get(w[1]).unwrap(), *mnam2id.get(w[3]).unwrap());
+            let op = match w[2] {
+                "+" => Operation::Add(aidx, bidx),
+                "-" => Operation::Sub(aidx, bidx),
+                "*" => Operation::Mul(aidx, bidx),
+                "/" => Operation::Div(aidx, bidx),
+                _ => unreachable!(),
+            };
+            ops.push(OpWithValue {op, val: None});
+        }
+    }
+
+    //dbg!(&mnams);
+    //dbg!(&mnam2id);
+    //dbg!(&ops);
+    //dbg!(v);
+
+    let root_idx = *mnam2id.get("root").unwrap();
+    let result = calc(root_idx, &mut ops);
+    println!("Monkey result: {result}");
 
     part1();
     part2();
