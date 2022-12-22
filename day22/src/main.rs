f59f2fd60cfd764d46b292457db0e0ba2892b30e
johanst <johan.sternerup@gmail.com>
2022-12-22 09:55:42 +0100

    drew the map

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -31,6 +31,39 @@ struct Actor {
     ydir : i32,
 }
 
+#[derive(Debug)]
+struct Config {
+    m : Vec<Vec<Place>>,
+    myrng : Vec<(usize,usize)>,
+    mxrng : Vec<(usize,usize)>,
+    xsize : usize,
+    ysize : usize,
+}
+
+fn draw_map(cfg : &Config, actor : &Actor) {
+    for y in 0..cfg.ysize {
+        for x in 0..cfg.xsize {
+            let c = if (x,y) == (actor.xpos as usize, actor.ypos as usize) {
+                match (actor.xdir, actor.ydir) {
+                    (1, 0) => '>',
+                    (-1, 0) => '<',
+                    (0, 1) => 'v',
+                    (0, -1) => '^',
+                    _ => unreachable!(),
+                }
+            } else {
+                match cfg.m[y][x] {
+                    Place::Outside => ' ',
+                    Place::Wall => '#',
+                    Place::Path => '.',
+                }
+            };
+            print!("{c}");
+        }
+        println!();
+    }
+}
+
 fn part1() {
 }
 
@@ -48,13 +81,17 @@ fn main() {
     let ysize = v.len() - 2;
 
     let xsize = v.iter().take(ysize).map(|r| r.len()).max().unwrap();
-    dbg!(ysize, xsize);
+    //dbg!(ysize, xsize);
 
-    let mut myrng : Vec<(usize,usize)> = Vec::new(); // given x, what is ymin,ymax
-    let mut mxrng : Vec<(usize,usize)> = Vec::new(); // given y, what is xmin,xmax
-    let mut m : Vec<Vec<Place>> = Vec::new();
+    let mut cfg = Config {
+        m : Vec::new(),
+        myrng : Vec::new(), // given x, what is ymin,ymax
+        mxrng : Vec::new(), // given y, what is xmin,xmax
+        xsize,
+        ysize,
+    };
     for (yidx, row) in v.iter().take(ysize).enumerate() {
-        m.push(Vec::new());
+        cfg.m.push(Vec::new());
         let mut xmin = usize::MAX;
         let mut xmax = usize::MIN;
         for (xidx, c) in row.chars().enumerate() {
@@ -64,33 +101,32 @@ fn main() {
                 '#' => Place::Wall,
                 _ => unreachable!(),
             };
-            m[yidx].push(place);
+            cfg.m[yidx].push(place);
             if place != Place::Outside {
                 xmin = cmp::min(xmin, xidx);
                 xmax = cmp::max(xmax, xidx);
             }
         }
-        let xlen = m[yidx].len();
+        let xlen = cfg.m[yidx].len();
         if xlen != xsize {
-            m[yidx].extend(iter::repeat(Place::Outside).take(xsize - xlen));
+            cfg.m[yidx].extend(iter::repeat(Place::Outside).take(xsize - xlen));
         }
-        mxrng.push((xmin, xmax));
+        cfg.mxrng.push((xmin, xmax));
     }
     // Calc yranges
     for xidx in 0..xsize {
         let mut ymin = usize::MAX;
         let mut ymax = usize::MIN;
         for yidx in 0..ysize {
-            if m[yidx][xidx] != Place::Outside {
+            if cfg.m[yidx][xidx] != Place::Outside {
                 ymin = cmp::min(ymin, yidx);
                 ymax = cmp::max(ymax, yidx);
             }
         }
-        myrng.push((ymin, ymax));
+        cfg.myrng.push((ymin, ymax));
     }
     // Get path
     let mut p : Vec<Move> = Vec::new();
-    let mut c_last = '?';
     let mut n = 0;
     for c in v.last().unwrap().chars() {
         match c {
@@ -114,11 +150,19 @@ fn main() {
         p.push(Move::Walk(n));
     }
 
-    dbg!(&p);
+    let actor = Actor {
+        xpos : cfg.mxrng[0].0 as i32,
+        ypos : 0,
+        xdir : 1,
+        ydir : 0,
+    };
+    draw_map(&cfg, &actor);
 
-    //dbg!(m);
-    //dbg!(mxrng);
-    //dbg!(myrng);
+    //dbg!(&p);
+
+    //dbg!(cfg.m);
+    //dbg!(cfg.mxrng);
+    //dbg!(cfg.myrng);
     //dbg!(v);
 
     part1();
