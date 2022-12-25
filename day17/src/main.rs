cc55ca49525299c798e87725bbd908ea490abedd
johanst <johan.sternerup@gmail.com>
2022-12-25 20:44:08 +0100

    finally day17

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -119,6 +119,33 @@ fn tower_height(cha : &VecDeque<Vec<u8>>) -> i64 {
     cha.len() as i64 - 1 - get_no_of_empty_rows(cha)
 }
 
+fn get_top_heights(cha : &VecDeque<Vec<u8>>) -> u64 {
+    let mut v : [u8; 7] = [255; 7];
+    for col in 1..8 {
+        for (idx, r) in cha.iter().rev().enumerate() {
+            if r[col] == b'#' {
+                v[col - 1] = idx as u8;
+                break;
+            }
+        }
+    }
+    let min = *v.iter().min().unwrap();
+    let mut r = 0;
+    for x in v.iter() {
+        r *= 256;
+        r +=  (*x - min) as u64
+    }
+
+    r
+}
+
+#[derive(Debug, Clone, Default)]
+struct TowerInfo {
+    height : i64,
+    rock_count : u64,
+    jet_count : u64,
+}
+
 fn main() {
     let lines = std::fs::read_to_string("shapes.txt").unwrap();
     let v = lines.split("\n\n").collect::<Vec<&str>>();
@@ -148,6 +175,10 @@ fn main() {
     let vc = "+-------+".as_bytes().to_vec();
     chamber.push_back(vc);
 
+    let mut twi0 = TowerInfo::default();
+    let mut twi1 = TowerInfo::default();
+    let mut twi2 = TowerInfo::default();
+
     //let knas = get_no_of_empty_rows(&chamber);
     //dbg!(knas);
 
@@ -156,6 +187,10 @@ fn main() {
     let mut jet_count : u64 = 0;
     let mut shitr = shapes.iter().cycle();
     let mut jetitr = v[0].chars().cycle();
+
+    // map jet_cycle -> map shape cycle
+    let mut cycle_finder : Vec<HashMap<(u32, u64),TowerInfo>> =
+        vec![HashMap::new(); v[0].len()];
     // 13345
     // for _ in 0..2022 {
     //for _ in 0..(5 * 2 * v[0].len() + 1) {
@@ -216,41 +251,72 @@ fn main() {
         }
 
         rock_count += 1;
-        if rock_count % 5 == 0 && jet_count % v[0].len() as u64 == 0 {
+
+        let height_map = get_top_heights(&chamber);
+
+        let jet_idx : usize = jet_count as usize % v[0].len();
+        let rock_key : (u32, u64) = (
+            (rock_count % 5) as u32, height_map);
+        if cycle_finder[jet_idx].contains_key(&rock_key) {
+            twi0 = cycle_finder[jet_idx].get(&rock_key).unwrap().clone();
+            twi1 = TowerInfo {
+                height : tower_height(&chamber) + rows_removed as i64,
+                rock_count,
+                jet_count
+            };
+            //dbg!(jet_count, rock_count, tower_height(&chamber));
+            //dbg!(cycle_finder[jet_idx].get(&rock_key).unwrap());
             break;
+        } else {
+            cycle_finder[jet_idx].insert(
+                rock_key,
+                TowerInfo {
+                    height : tower_height(&chamber) + rows_removed as i64,
+                    rock_count,
+                    jet_count
+                });
         }
 
-        if rock_count == 1_000_000_000_000 {
-            break;
+        if rock_count >= 98 + 42 && twi2.height == 0 {
+            twi2 = TowerInfo {
+                height : tower_height(&chamber) + rows_removed as i64,
+                rock_count,
+                jet_count
+            };
         }
 
-        if rock_count % 1_000_000 == 0 {
-            println!("{rock_count} {jet_count}");
-        }
+//        if rock_count == 98 {
+//            println!("At rock count 98, heights = {:X}", height_map);
+//            print_chamber_last_15(&chamber);
+//        }
 
-        //let jet = jetitr.next().unwrap();
     }
 
     print_chamber_last_15(&chamber);
     println!("Rock count: {}", rock_count);
     println!("Tower height: {}", tower_height(&chamber) + rows_removed as i64);
 
-    // 79089 height for 5 * 10091 (jet length * shape )
-    let ncyc : u64 = 1000000000000 / ( 5 * 10091 ); // 19819641
-    let rcyc : u64 = 1000000000000 % ( 5 * 10091 ); // 13345
-    // 20945 height for remainder 13345
+    dbg!(&twi0);
+    dbg!(&twi1);
+    dbg!(&twi2);
 
-    // 608 height for 5 * 2 * 40 ( jet length * shape)
-    let exncyc : u64 = 1000000000000 / ( 5 * 2 * 40 ); // 2500000000
-    let exrcyc : u64 = 1000000000000 % ( 5 * 2 * 40 ); //
+    let ncyc : u64 = (1000000000000 - twi0.rock_count) / (
+        twi1.rock_count - twi0.rock_count);
+    let rcyc : u64 = (1000000000000 - twi0.rock_count) % (
+        twi1.rock_count - twi0.rock_count);
 
     dbg!(ncyc, rcyc);
-    dbg!(exncyc, exrcyc);
-    let total : u64 = 19819641 * 79089 + 20945;
+//    dbg!(exncyc, exrcyc);
+//    let total : u64 = 19819641 * 79089 + 20945;
+//    dbg!(total);
+//    let extotal : u64 = 2500000000 * 608;
+//    dbg!(extotal);
+    //    // 1567515607994 too low
+
+    let total : u64 = twi0.height as u64 +
+        ncyc * (twi1.height - twi0.height) as u64 +
+        (twi2. height - twi0.height) as u64;
     dbg!(total);
-    let extotal : u64 = 2500000000 * 608;
-    dbg!(extotal);
-    // 1567515607994 too low
 
     part1();
     part2();
